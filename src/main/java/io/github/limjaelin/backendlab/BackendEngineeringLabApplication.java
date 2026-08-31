ec94f8ce7fab706780f2795c8dc58d7dc4193bd4
임재린 (Jaelin Lim) <90305701+LIMJAELIN@users.noreply.github.com>
2026-08-31 17:35:06 +0900

    feat: add Spring Boot application

---
+++ src/main/java/io/github/limjaelin/backendlab/BackendEngineeringLabApplication.java
@@ -0,0 +1,12 @@
+package io.github.limjaelin.backendlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEngineeringLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEngineeringLabApplication.class, args);
+    }
+}
